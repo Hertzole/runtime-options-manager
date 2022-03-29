8e168584c41e42e5299ce6db0017655a19e9ff6e
Hertzole <hampusringstrom@gmail.com>
2022-03-29 21:31:47 +0200

    Added input rebinding

---
+++ Editor/Settings/SettingEditor.cs
@@ -0,0 +1,53 @@
+﻿using UnityEditor;
+
+namespace Hertzole.Settings.Editor
+{
+	[CustomEditor(typeof(Setting))]
+	public class SettingEditor : UnityEditor.Editor
+	{
+		// General Settings
+		private SerializedProperty displayName;
+#if HERTZ_SETTINGS_LOCALIZATION
+		private SerializedProperty displayNameLocalized;
+#endif
+		private SerializedProperty identifier;
+
+		// UI Settings
+		private SerializedProperty uiPrefab;
+#if HERTZ_SETTINGS_UIELEMENTS
+		private SerializedProperty uiElement;
+#endif
+
+		protected virtual void OnEnable()
+		{
+			displayName = serializedObject.FindProperty(nameof(displayName));
+#if HERTZ_SETTINGS_LOCALIZATION
+			displayNameLocalized = serializedObject.FindProperty(nameof(displayNameLocalized));
+#endif
+			identifier = serializedObject.FindProperty(nameof(identifier));
+			
+			uiPrefab = serializedObject.FindProperty(nameof(uiPrefab));
+#if HERTZ_SETTINGS_UIELEMENTS
+			uiElement = serializedObject.FindProperty(nameof(uiElement));
+#endif
+		}
+
+		public override void OnInspectorGUI()
+		{
+			serializedObject.Update();
+			
+			EditorGUILayout.PropertyField(displayName);
+#if HERTZ_SETTINGS_LOCALIZATION
+			EditorGUILayout.PropertyField(displayNameLocalized);
+#endif
+			EditorGUILayout.PropertyField(identifier);
+			
+			EditorGUILayout.PropertyField(uiPrefab);
+#if HERTZ_SETTINGS_UIELEMENTS
+			EditorGUILayout.PropertyField(uiElement);
+#endif
+
+			serializedObject.ApplyModifiedProperties();
+		}
+	}
+}
