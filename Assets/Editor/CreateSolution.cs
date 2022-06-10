39db2d70dc1d4b9214aa78c2af971a0c655fc1f7
Hertzole <hampusringstrom@gmail.com>
2022-06-10 23:08:31 +0200

    New way of creating a solution

--- Assets/Editor/CreateSolution.cs
+++ Assets/Editor/CreateSolution.cs
@@ -1,14 +1,19 @@
-using Unity.CodeEditor;
+#if UNITY_EDITOR
+
+using Microsoft.Unity.VisualStudio.Editor;
 using UnityEditor;
 
 namespace GitTools
 {
-	public static class CreateSolution
+	public static class Solution
 	{
+		[MenuItem("Tools/Sync")]
 		public static void Sync()
 		{
+			ProjectGeneration projectGeneration = new ProjectGeneration();
 			AssetDatabase.Refresh();
-			CodeEditor.CurrentEditor.SyncAll();
+			projectGeneration.GenerateAndWriteSolutionAndProjects();
 		}
 	}
 }
+#endif
