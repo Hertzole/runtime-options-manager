645daf3467cf176922005ed74301bd36c793ad4e
Hertzole <hampusringstrom@gmail.com>
2022-08-05 13:24:04 +0200

    add: option to decide how language names are displayed

---
+++ Packages/se.hertzole.runtime-options-manager/Runtime/Data/SerializableKeyValuePair.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hertzole.OptionsManager
+{
+	[Serializable]
+	public struct SerializableKeyValuePair<TKey, TValue> : IEquatable<SerializableKeyValuePair<TKey, TValue>>
+	{
+		public TKey key;
+		public TValue value;
+
+		public SerializableKeyValuePair(TKey key, TValue value)
+		{
+			this.key = key;
+			this.value = value;
+		}
+
+		public bool Equals(SerializableKeyValuePair<TKey, TValue> other)
+		{
+			return EqualityComparer<TKey>.Default.Equals(key, other.key) && EqualityComparer<TValue>.Default.Equals(value, other.value);
+		}
+
+		public override bool Equals(object obj)
+		{
+			return obj is SerializableKeyValuePair<TKey, TValue> other && Equals(other);
+		}
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				return (EqualityComparer<TKey>.Default.GetHashCode(key) * 397) ^ EqualityComparer<TValue>.Default.GetHashCode(value);
+			}
+		}
+
+		public static bool operator ==(SerializableKeyValuePair<TKey, TValue> left, SerializableKeyValuePair<TKey, TValue> right)
+		{
+			return left.Equals(right);
+		}
+
+		public static bool operator !=(SerializableKeyValuePair<TKey, TValue> left, SerializableKeyValuePair<TKey, TValue> right)
+		{
+			return !left.Equals(right);
+		}
+	}
+}
