a32d2d4954dd02133ac9778d9485e36fafdb439f
Hertzole <hampusringstrom@gmail.com>
2022-08-05 23:40:23 +0200

    add: a bunch of tests

---
+++ Packages/se.hertzole.runtime-options-manager/Tests/Runtime/Localization/LanguageValues.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+using Assert = UnityEngine.Assertions.Assert;
+
+namespace Hertzole.OptionsManager.Tests
+{
+	public class LanguageValues : LocalizationTest
+	{
+		[Test]
+		public void GetSerializableValue_NullValue()
+		{
+			LanguageSetting setting = AddSetting<LanguageSetting>();
+			setting.DefaultValue = Locales[0];
+			setting.Value = null;
+
+			Assert.AreEqual(Locales[0].Identifier.Code, setting.GetSerializeValue());
+		}
+
+		[Test]
+		public void GetSerializableValue_NotNullValue()
+		{
+			LanguageSetting setting = AddSetting<LanguageSetting>();
+			setting.DefaultValue = Locales[0];
+			setting.Value = Locales[1];
+
+			Assert.AreEqual(Locales[1].Identifier.Code, setting.GetSerializeValue());
+		}
+
+		[Test]
+		public void SetSerializableValue_Code()
+		{
+			LanguageSetting setting = AddSetting<LanguageSetting>();
+
+			setting.SetSerializedValue(Locales[0].Identifier.Code, new JsonSettingSerializer());
+			
+			Assert.AreEqual(Locales[0].Identifier, setting.Value.Identifier);
+			Assert.AreEqual(TargetLocalizationSettings.GetSelectedLocale().Identifier, setting.Value.Identifier);
+		}
+		
+		[Test]
+		public void SetSerializableValue_Locale()
+		{
+			LanguageSetting setting = AddSetting<LanguageSetting>();
+
+			setting.SetSerializedValue(Locales[0], new JsonSettingSerializer());
+			
+			Assert.AreEqual(Locales[0].Identifier, setting.Value.Identifier);
+			Assert.AreEqual(TargetLocalizationSettings.GetSelectedLocale().Identifier, setting.Value.Identifier);
+		}
+		
+		[Test]
+		public void SetSerializableValue_Null()
+		{
+			LanguageSetting setting = AddSetting<LanguageSetting>();
+
+			setting.SetSerializedValue(null, new JsonSettingSerializer());
+			
+			Assert.AreEqual(TargetLocalizationSettings.GetSelectedLocale().Identifier, setting.Value.Identifier);
+		}
+		
+		[Test]
+		public void SetSerializableValue_Invalid()
+		{
+			LanguageSetting setting = AddSetting<LanguageSetting>();
+
+			setting.SetSerializedValue(42, new JsonSettingSerializer());
+			
+			Assert.AreEqual(TargetLocalizationSettings.GetSelectedLocale().Identifier, setting.Value.Identifier);
+		}
+	}
+}
---
+++ Packages/se.hertzole.runtime-options-manager/Tests/Runtime/ResolutionTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using Assert = UnityEngine.Assertions.Assert;
+
+namespace Hertzole.OptionsManager.Tests
+{
+	public class ResolutionTests : BaseTest
+	{
+		[Test]
+		public void GetSet_DropdownValue()
+		{
+			ResolutionSetting setting = AddSetting<ResolutionSetting>();
+			Resolution[] resolutions = setting.GetUniqueResolutions();
+			for (int i = 0; i < resolutions.Length; i++)
+			{
+				setting.SetDropdownValue(i);
+
+				Assert.AreEqual(resolutions[i].width, resolutions[setting.GetDropdownValue()].width);
+				Assert.AreEqual(resolutions[i].height, resolutions[setting.GetDropdownValue()].height);
+			}
+		}
+
+		[Test]
+		public void Set_InvalidSerializableValue()
+		{
+			ResolutionSetting setting = AddSetting<ResolutionSetting>();
+			setting.SetSerializedValue(42, new JsonSettingSerializer());
+			Assert.AreEqual(Screen.currentResolution, setting.Value);
+		}
+
+		[Test]
+		public void Get_DropdownValues()
+		{
+			ResolutionSetting setting = AddSetting<ResolutionSetting>();
+			setting.ResolutionFormat = "{0} and {1}";
+			var resolutions = setting.GetUniqueResolutions();
+			var dropdownValues = setting.GetDropdownValues();
+			Assert.AreEqual(resolutions.Length, dropdownValues.Count);
+
+			for (int i = 0; i < dropdownValues.Count; i++)
+			{
+				Assert.AreEqual($"{resolutions[i].width} and {resolutions[i].height}", dropdownValues[i].text);
+			}
+		}
+	}
+}
