949ebd7682c850e97fc65e173d75d3dfd5b19abf
Hertzole <hampusringstrom@gmail.com>
2022-08-05 20:47:17 +0200

    fix: GetHashCode referencing mutable fields

--- Runtime/Data/SerializableKeyValuePair.cs
+++ Runtime/Data/SerializableKeyValuePair.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace Hertzole.OptionsManager
 {
 	[Serializable]
 	public struct SerializableKeyValuePair<TKey, TValue> : IEquatable<SerializableKeyValuePair<TKey, TValue>>
 	{
-		public TKey key;
-		public TValue value;
+		[SerializeField]
+		private TKey key;
+		[SerializeField]
+		private TValue value;
+
+		public TKey Key { get { return key; } }
+		public TValue Value { get { return value; } }
 
 		public SerializableKeyValuePair(TKey key, TValue value)
 		{
@@ -15,21 +21,11 @@ namespace Hertzole.OptionsManager
 			this.value = value;
 		}
 
-		public bool Equals(SerializableKeyValuePair<TKey, TValue> other)
-		{
-			return EqualityComparer<TKey>.Default.Equals(key, other.key) && EqualityComparer<TValue>.Default.Equals(value, other.value);
-		}
-
-		public override bool Equals(object obj)
-		{
-			return obj is SerializableKeyValuePair<TKey, TValue> other && Equals(other);
-		}
-
 		public override int GetHashCode()
 		{
 			unchecked
 			{
-				return (EqualityComparer<TKey>.Default.GetHashCode(key) * 397) ^ EqualityComparer<TValue>.Default.GetHashCode(value);
+				return (EqualityComparer<TKey>.Default.GetHashCode(Key) * 397) ^ EqualityComparer<TValue>.Default.GetHashCode(Value);
 			}
 		}
 
@@ -41,6 +37,16 @@ namespace Hertzole.OptionsManager
 		public static bool operator !=(SerializableKeyValuePair<TKey, TValue> left, SerializableKeyValuePair<TKey, TValue> right)
 		{
 			return !left.Equals(right);
+		}
+
+		public override bool Equals(object obj)
+		{
+			return obj is SerializableKeyValuePair<TKey, TValue> other && Equals(other);
+		}
+
+		public bool Equals(SerializableKeyValuePair<TKey, TValue> other)
+		{
+			return EqualityComparer<TKey>.Default.Equals(key, other.key) && EqualityComparer<TValue>.Default.Equals(value, other.value);
 		}
 	}
 }
